b4f9425636948c6ec085685943e143623b929c6d
Alex Pilon <apilon@hashicorp.com>
2020-07-06 21:13:19 -0400

    Upgrade to TF SDK V2

--- aws/internal/service/ec2/waiter/status.go
+++ aws/internal/service/ec2/waiter/status.go
@@ -3,7 +3,7 @@ package waiter
 import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/ec2"
-	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 )
 
 // LocalGatewayRouteTableVpcAssociationState fetches the LocalGatewayRouteTableVpcAssociation and its State
